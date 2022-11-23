284cdfd935561aa46ad9b5b3b428fe1470410572
Javier <jgreen_18@alu.uabcs.mx>
2022-11-23 00:48:32 -0700

    mapas v1

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,5 +1,7 @@
 <x-app-layout>
-    
-    @livewire('prueba');
-    
+     <div class="w-1/2 h-1/2 ">
+         <x-maps-leaflet :markers="[['lat' => 24.11623, 'long' => -110.31029]]" :zoomLevel="15" :centerPoint="['lat' => 24.11623, 'long' => -110.31029]"></x-maps-leaflet> 
+
+     </div>
+
 </x-app-layout>
