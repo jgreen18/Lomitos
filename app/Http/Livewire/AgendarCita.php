c5772d0d098ee0fa1e720eb4aeb5ee19f6de92f5
Javier <jgreen_18@alu.uabcs.mx>
2022-11-26 01:17:45 -0700

    validacion mascotas

--- app/Http/Livewire/AgendarCita.php
+++ app/Http/Livewire/AgendarCita.php
@@ -4,13 +4,17 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Mascotas;
+use Illuminate\Support\Facades\Auth;
 
 class AgendarCita extends Component
 {
     public $open = false;    
     public function render()
     {
-        $mascotas = Mascotas::all();
-        return view('livewire.agendar-cita', compact('mascotas'));
+        $id = Auth::user()->id;
+        $mascotas = Mascotas::where('user_id' ,'=', $id)->get();
+        
+        
+        return view('livewire.agendar-cita', compact('mascotas') );
     }
 }
